e0bca4a878972d15b5cbfbb3af8c912435dad1c2
Serg Minin <everestdon@ukr.net>
2019-03-13 20:59:05 +0300

    добавил прием данных спом. fetch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React from "react";
+import { useFetch } from "./hooks";
 import TypicalBlock from "./components/TypicalBlock";
-import { typeCatsFood } from "./data";
 
 import "./App.css";
 
 const App = () => {
+  const [dataFoods, loading] = useFetch("/catsFood.json");
+
   return (
     <div className="App">
       <div className="App_container">
         <h1>Ты сегодня покормил кота?</h1>
         <div className="App_wrap-blocks">
-          {typeCatsFood.map(item => {
-            return (
-              <TypicalBlock
-                key={item.nameType}
-                nameType={item.nameType}
-                sumOfDose={item.sumOfDose}
-                present={item.present}
-                massa={item.massa}
-                dopMessage={item.dopMessage}
-                status={item.status}
-                disabled={item.disabled}
-                footerActiveText={item.footerActiveText}
-              />
-            );
-          })}
+          {loading ? (
+            <p className="App_loading">"Загрузка..."</p>
+          ) : (
+            dataFoods.map(item => {
+              const { ...itemProps } = item;
+              return (
+                <TypicalBlock
+                  // key={item.nameType}
+                  // nameType={item.nameType}
+                  // sumOfDose={item.sumOfDose}
+                  // present={item.present}
+                  // massa={item.massa}
+                  // dopMessage={item.dopMessage}
+                  // status={item.status}
+                  // disabled={item.disabled}
+                  // footerActiveText={item.footerActiveText}
+                  {...itemProps}
+                />
+              );
+            })
+          )}
         </div>
       </div>
     </div>
